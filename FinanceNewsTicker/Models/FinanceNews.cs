53840c645b3aba1ec308b1ab206f07945bf6f7ba
JannickL <jannick.leismann@gmx.de>
2022-04-06 09:41:02 +0200

    commited project

---
+++ FinanceNewsTicker/Models/FinanceNews.cs
@@ -0,0 +1,54 @@
+﻿using Newtonsoft.Json;
+
+namespace FinanceNewsTicker.Models
+{
+
+    public class Pagination
+    {
+        [JsonProperty("limit")]
+        public int Limit { get; set; }
+
+        [JsonProperty("offset")]
+        public int Offset { get; set; }
+
+        [JsonProperty("count")]
+        public int Count { get; set; }
+
+        [JsonProperty("total")]
+        public int Total { get; set; }
+    }
+
+    public class NewsArticle
+    {
+        [JsonProperty("title")]
+        public string Title { get; set; }
+
+        [JsonProperty("url")]
+        public string Url { get; set; }
+
+        [JsonProperty("description")]
+        public string Description { get; set; }
+
+        [JsonProperty("source")]
+        public string Source { get; set; }
+
+        [JsonProperty("tickers")]
+        public List<string> Tickers { get; set; }
+
+        [JsonProperty("tags")]
+        public List<string> Tags { get; set; }
+
+        [JsonProperty("published_at")]
+        public DateTime PublishedAt { get; set; }
+    }
+
+    public class FinanceNews
+    {
+        [JsonProperty("pagination")]
+        public Pagination Pagination { get; set; }
+
+        [JsonProperty("data")]
+        public List<NewsArticle> Data { get; set; }
+    }
+
+}
